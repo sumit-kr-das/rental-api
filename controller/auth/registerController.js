5c7a826e0c385068e5930b0b3a8e11d871b62915
sumit-kr-das <sumitkrdas777@gmail.com>
2022-10-29 00:05:34 +0530

    update registerController.js
    salt_round updated

--- controller/auth/registerController.js
+++ controller/auth/registerController.js
@@ -1,12 +1,14 @@
 import UserSchema from '../../models/UserSchema.js';
+import { SALT_ROUND } from '../../config/index.js';
 import bcrypt from 'bcrypt';
 
 
 const registerController = {
     async register(req,res,next) {
         const { name, email, password } = req.body;
+        const saltRound = Number(SALT_ROUND);
         try{
-            const salt = bcrypt.genSaltSync(10);
+            const salt = bcrypt.genSaltSync(saltRound);
             const hash = bcrypt.hashSync(password, salt);
 
             const newUser = new UserSchema({ name, email, password: hash });
