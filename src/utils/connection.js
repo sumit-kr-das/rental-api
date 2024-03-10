9fe6b0ce65df2237a82cbf823320efbedef06aa7
sumit-kr-das <sumitkrdas777@gmail.com>
2024-03-10 23:25:08 +0530

    mongo connection deprecation error solved

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,18 +1,19 @@
 import mongoose from "mongoose";
-import { DB_URI } from '../config/index.js';
+import { DB_URI } from "../config/index.js";
 
 mongoose.connection.on("connected", () => {
-    console.log("DB Connected...");
-})
+  console.log("DB Connected...");
+});
 
 mongoose.connection.on("disconnected", () => {
-    console.log("DB Disconnected ..x..x..x..");
-})
+  console.log("DB Disconnected ..x..x..x..");
+});
 
 export default async function connection() {
-    try{
-        await mongoose.connect(DB_URI);
-    }catch(err){
-        next(err);
-    }
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(DB_URI);
+  } catch (err) {
+    next(err);
+  }
 }
