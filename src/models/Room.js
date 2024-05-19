d7c1a52fa27f0e57f46aebb8e26f77fe92f20315
sumit-kr-das <sumitkrdas777@gmail.com>
2024-05-19 21:49:10 +0530

    hotel controller updated

--- src/models/Room.js
+++ src/models/Room.js
@@ -1,17 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const reqString = { type: String, required: true };
 const reqNumber = { type: Number, required: true };
 
-const RoomSchema = new mongoose.Schema({
+const RoomSchema = new mongoose.Schema(
+  {
+    // hotel: { type: mongoose.Schema.Types.ObjectId, ref: "Hotels" },
     title: reqString,
     price: reqNumber,
     beds: reqNumber,
     maxPeople: reqNumber,
     area: { type: Number },
     desc: reqString,
-    roomNumbers: [{ number: Number, unAavailableDates: { type: [ Date ]} }],
-}, { timestamps: true });
+    roomNumbers: [{ number: Number, unAavailableDates: { type: [Date] } }],
+  },
+  { timestamps: true }
+);
 
 /*
     [
