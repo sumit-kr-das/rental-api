5603c9e3059cd74371f8d86c587c5c7d8164780e
sumit-kr-das <sumitkrdas777@gmail.com>
2024-05-20 00:07:46 +0530

    booking find by id property changed

--- src/controller/hotelController.js
+++ src/controller/hotelController.js
@@ -106,8 +106,6 @@ const hotelController = {
           //   });
           // }
           const newUploadedImages = await Promise.all(removeUploadedImages);
-          console.log("from update", newUploadedImages);
-          console.log("Image is deleted from cloudnary");
         }
 
         /* insert new images */
@@ -167,7 +165,6 @@ const hotelController = {
   async getHotel(req, res, next) {
     try {
       const getSingleHotel = await Hotel.findById(req.params.id);
-      console.log("Hotel find by id", getSingleHotel);
       res.status(200).json(getSingleHotel);
     } catch (err) {
       next(err);
@@ -185,7 +182,6 @@ const hotelController = {
 
   async getHotels(req, res, next) {
     const { min, max, limit, ...others } = req.query;
-    console.log(others);
     try {
       const getAllHotels = await Hotel.find({
         ...others,
